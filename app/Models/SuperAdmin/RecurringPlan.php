da585d340a632c699c33f12b5461e5ff43260097
musamakhizar07 <ukhizer@azlaantechnologies.com>
2025-01-13 16:59:19 -0700

    Database Schema

---
+++ app/Models/SuperAdmin/RecurringPlan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\SuperAdmin;
+
+use Illuminate\Database\Eloquent\Model;
+
+//If Recurring Plan Then Use This Model : This is optional if you want to use it then this relation is created and used
+class RecurringPlan extends Model
+{
+    //
+}
